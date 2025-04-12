42ef064acea5dd8ed4d87ccf60143112a78483da
Quan Pham <quan.phamvt040103@hcmut.edu.vn>
2025-04-12 16:46:53 +0700

    update node.cpp with function handleProcessBuffer

--- sendrecvLoRa/node.cpp
+++ sendrecvLoRa/node.cpp
@@ -1,68 +1,15 @@
 #include "globals.h"
 
-class NodeStatus {
-public:
-  int address;
-  int state;
-  int pwm_val;
-  NodeStatus(int add = -1, int state = 0, int val = -1)
-  {
-    this->address = add;
-    this->state = state;
-    this->pwm_val = val;
-  }
-};
-
-NodeStatus deserializeJsonFormat(const String &dataraw)
+// Handle after receive lora package from gateway
+void handleProcessBuffer(void *pvParameters)
 {
-  NodeStatus node;
-  JsonDocument doc;
-  DeserializationError error = deserializeJson(doc, dataraw);
-
-  if (error)
+  BasicQueue<String> *q = (BasicQueue<String> *)pvParameters;
+  while(1)
   {
-      printData("[ERROR] deserializeJson() failed: ");
-      printlnData(error.f_str());
-      return node;
-  }
-
-  // get value from Json message
-  String address = doc["Address"].as<String>(); // _001 -> 1
-  JsonObject data = doc["data"].as<JsonObject>();
-  String state = data["Relay"].as<String>();
-  String pwm_val = data["PWM"].as<String>();
-  
-  // Pass value to Node's attributes
-  node.address = address.toInt();
-  node.pwm_val = pwm_val.toInt();
-  if (state == "high") // do server gui ve qui dinh high/low
-    node.state = 1;
-  if (state == "low")
-    node.state = 0;
-
-  Serial.println("Deserialize data success.");
-  return node;
-}
-
-void LoRaRecvTask(void *pvParameters)
-{
-  String data_buffer;   // Simple buffer for received data
-  RecvFrame_t data;
-  Serial.println("Waiting for new Package coming ...");
-  while (1)
-  {
-    if (getLoraIns()->RecieveFrame(&data) == 0)
+    if (!q->isEmpty())
     {
-      data_buffer = String(data.recv_data, data.recv_data_len);
-      Serial.print("Data size: ");
-      Serial.println(data.recv_data_len);
-      Serial.print(data_buffer);
-
-      Serial.println();
-      Serial.printf("RSSI: %d dBm\n", data.rssi);
-      Serial.flush();
-
-      NodeStatus node = deserializeJsonFormat(data_buffer);
+      NodeStatus node;
+      node = deserializeJsonFormat(q->pop());
       // Check destination address
       if (node.address != getConfigLora()->own_address)
       {
@@ -92,6 +39,7 @@ void LoRaRecvTask(void *pvParameters)
         }
       } 
     }
+    vTaskDelay(pdMS_TO_TICKS(delay_process_buffer));
   }
   vTaskDelete(nullptr);
 }
@@ -118,7 +66,6 @@ void LoRaRecvTask(void *pvParameters)
 //   }
 // }
 
-
 /* Setup function */
 void setup()
 {
@@ -140,9 +87,10 @@ void setup()
 
   // Initialize Network layer and Device layer
   device_init();
-
+  BasicQueue<String> *buffer = new BasicQueue<String>();
   // Create task for RTOS
-  xTaskCreate(LoRaRecvTask, "Test Node receive", 1024 * 8, nullptr, 0, nullptr);
+  xTaskCreate(handleProcessBuffer, "handle process buffer", 1024 * 8, buffer, 1, nullptr);
+  xTaskCreate(LoRaRecvTask, "rcv", 1024*4, buffer, 0, nullptr);
   xTaskCreate(readDataDHT20, "DHT20 data reader", 1024 * 4, nullptr, 1, nullptr);
 
   digitalWrite(INBUILD_LED_PIN, HIGH);    // Turn on the LED when set up completely
